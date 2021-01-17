9ff8b2496502987327e8c43432f386439b1bfedf
Samuel Cabezas <samuel.cabezas.632@gmail.com>
2021-01-17 17:20:15 -0300

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
-app.get("/", (req, res) => res.send("Esto es una prueba!"));
+app.get("/", (req, res) => res.send("Probando deploy!"));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
